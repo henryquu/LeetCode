4e567e9b67b1a570f04f05867bda8babbd179fb2
henryquu <malve1275@gmail.com>
2022-12-30 21:32:05 +0100

    Time: 3684 ms (24.49%), Space: 13.9 MB (89.87%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -4,10 +4,9 @@ class Solution:
         m = s[0]
         for i in range(n):
             for j in range(i + 1, n):
-                if s[i] == s[j]:
+                if j - i + 1 > len(m) and s[i] == s[j]:
                     sub = s[i:j + 1]
                     if sub == sub[::-1]:
-                        if j - i + 1 > len(m):
-                            m = sub
+                        m = sub
 
         return m
