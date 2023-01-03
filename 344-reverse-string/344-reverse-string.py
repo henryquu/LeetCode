455eb39729803e3990848c599e07093567b98e28
henryquu <malve1275@gmail.com>
2023-01-03 18:49:25 +0100

    Time: 375 ms (51.61%), Space: 18.5 MB (41.26%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,12 +3,7 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        l = 0
-        r = len(s) - 1
-
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
+        for i in range(len(s) // 2):
+            s[i], s[-i - 1] = s[-i - 1], s[i]
         
             
