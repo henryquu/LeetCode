15d63d9f0bee92174a3f2e9d329b67a4377f28b5
henryquu <malve1275@gmail.com>
2023-01-08 18:52:50 +0100

    Time: 1527 ms (54.95%), Space: 14 MB (59.47%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -2,20 +2,16 @@ class Solution:
         def longestPalindrome(self, s):
             modified = '|' + '|'.join(s) + '|'
 
-            center = 0
-            radius = 0
             longest = s[0]
-
             n = len(modified)
+            
             for center in range(1, n - 1):
+                radius = 0
                 while radius + 1 <= center and radius + 1 + center < n and modified[center - radius - 1] == modified[center + radius + 1]:
                     radius += 1
 
                 if radius * 2 + 1 > len(longest):
                     longest = modified[center - radius: center + radius + 1]
-                center += 1
-                radius = 0
 
             longest = ''.join([a for a in longest if a != '|'])
-
             return longest
